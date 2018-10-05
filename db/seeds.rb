a4623c5494e732ca6f4e998308d07131aedec9c5
Bakang <tech-a37@Tech-A37s-MacBook-Air.local>
2018-10-05 12:28:34 +0100

    edited descriptions for the subjects

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Subject.create(name: 'HTML', desc: 'HTML stands for Hyper Text Markup Language. HTML describes the structure of Web pages using markup. HTML elements are the building blocks of HTML pages.', img: 'html.png')
-Subject.create(name: 'CSS', desc: 'CSS stands for Cascading Style Sheets. CSS describes how HTML elements are to be displayed on screen, paper, or in other media.', img:'css.jpg')
-Subject.create(name: 'JavaScript', desc: 'JavaScript is most commonly used as a client side scripting language. This means that JavaScript code is written into an HTML page. When a user requests an HTML page with JavaScript in it, the script is sent to the browser and it\'s up to the browser to do something with it.', img:'js.jpg')
+Subject.create(name: 'HTML', desc: 'HTML stands for Hyper Text Markup Language. HTML describes the structure of Web pages using markup and the elements are the building blocks of your page.', img: 'html.png')
+Subject.create(name: 'CSS', desc: 'CSS stands for Cascading Style Sheets. CSS describes how HTML elements are displayed on screen, paper, or in other media.', img:'css.jpg')
+Subject.create(name: 'JavaScript', desc: 'JavaScript is most commonly used as a client side scripting language. This means that JavaScript code is written into an HTML page and when a user requests an HTML page with JavaScript in it, the script is sent to the browser and it\'s up to the browser to do something with it.', img:'js.jpg')
 Subject.create(name: 'Ruby', desc: 'Ruby is a general purpose programming language typically used for web development.Ruby developers frequently perform tasks like storing information in a database',img: 'ruby.png')
