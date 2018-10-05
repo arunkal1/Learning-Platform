5810d59fc48b818d1e5680b9697b149c76650daa
tech-a49 <tech-a49@Tech-A49s-MacBook-Air.local>
2018-10-05 11:29:45 +0100

    added controller for topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,22 +1,59 @@
 class TopicsController < ApplicationController
-  def index
-  end
 
-  def show
+  # before_action :set_subject
+
+  def index
+    @topics = Topic.all
   end
 
   def new
+    @topic = Topic.new
   end
 
   def edit
+    @topic = Topic.find params[:id]
   end
 
   def create
+    # Create a new topic related to the subject
+    # @topic = @subject.topic.create(topic_params)
+
+    # Development not related to subject
+    @topic = Topic.new topic_params
+
+
+    @topic.save
+    # redirect_to @subject
+
+
+    # development
+    redirect_to @topic
   end
 
   def update
+    @topic = Topic.find params[:id]
+    @topic.update topic_params
+    # redirect_to @subject
+
+    # development
+    redirect_to @topics
+
   end
 
   def destroy
+    @topic = Topic.find params[:id]
+    @topic.destroy
+    # redirect_to @subject
+
+    # development
+    redirect_to @topics
   end
+
+  def topic_params
+    topic_params = params.require(:topic).permit(:name,:desc,:img,:subject_id)
+  end
+
+  # def set_subject
+    # @subject = Subject.find params[:subject_id]
+  # end
 end
